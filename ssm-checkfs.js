798d7955664d3e7f01472e7fea183bd09f124d26
Philip Colmer <philip.colmer@linaro.org>
2020-11-27 14:25:57 +0000

    New script

---
+++ ssm-checkfs.js
@@ -0,0 +1,185 @@
+/* global input, http, output */
+
+const jsSHA = require('jsSHA');
+
+const hmacSha256 = (signingKey, stringToSign, type="HEX") => {
+    var sha_ob = new jsSHA("SHA-256", "TEXT");
+    sha_ob.setHMACKey(signingKey, type);
+    sha_ob.update(stringToSign);
+    return sha_ob.getHMAC("HEX");
+};
+
+function hashSha256(stringToHash) {
+    var sha_ob = new jsSHA('SHA-256', "TEXT");
+    sha_ob.update(stringToHash);
+    return sha_ob.getHash("HEX");
+}
+
+function prependLeadingZeroes(n) {
+    if (n <= 9) {
+        return "0" + n;
+    }
+    return n.toString();
+}
+
+function getSignatureKey(key, dateStamp, regionName, serviceName) {
+    var kDate = hmacSha256(`AWS4${key}`, dateStamp, "TEXT");
+    var kRegion = hmacSha256(kDate, regionName);
+    var kService = hmacSha256(kRegion, serviceName);
+    var kSigning = hmacSha256(kService, "aws4_request");
+    return kSigning;
+}
+
+function buildHeader(access_key, secret_key, region, amzTarget, payload) {
+    const method = "POST";
+    const service = "ssm";
+    const host = service+"."+region+".amazonaws.com";
+    const contentType = 'application/x-amz-json-1.1; charset=UTF-8';
+
+    const t = new Date();
+    const datestamp = `${t.getFullYear()}${prependLeadingZeroes(t.getMonth()+1)}${prependLeadingZeroes(t.getDate())}`;
+    // 4-digit year, 2-digit month, 2-digit date, T, 2-digit hour, 2-digit minutes, 2-digit seconds, Z
+    const amzdate = datestamp+"T"+prependLeadingZeroes(t.getHours())+prependLeadingZeroes(t.getMinutes())+prependLeadingZeroes(t.getSeconds())+"Z";
+    const canonical_uri = "/";
+    const canonical_querystring = "";
+    const canonical_headers = `content-type:${contentType}\nhost:${host}\nx-amz-date:${amzdate}\nx-amz-target:${amzTarget}\n`;
+    const signed_headers = 'content-type;host;x-amz-date;x-amz-target';
+    // Calculate the hash of the payload which, for GET, is empty
+    const payload_hash = hashSha256(payload);
+    const canonical_request = method + '\n' + canonical_uri + '\n' + canonical_querystring + '\n' + canonical_headers + '\n' + signed_headers + '\n' + payload_hash;
+    const algorithm = 'AWS4-HMAC-SHA256';
+    const credential_scope = datestamp + '/' + region + '/' + service + '/' + 'aws4_request';
+    const string_to_sign = algorithm + '\n' +  amzdate + '\n' +  credential_scope + '\n' +  hashSha256(canonical_request);
+    const signing_key = getSignatureKey(secret_key, datestamp, region, service);
+    const signature = hmacSha256(signing_key, string_to_sign).toString('hex');
+    const authorization_header = algorithm + ' ' + 'Credential=' + access_key + '/' + credential_scope + ', ' +  'SignedHeaders=' + signed_headers + ', ' + 'Signature=' + signature;
+    
+    var requestHeaders = {
+        "host": host,
+        "x-amz-date": amzdate,
+        "x-amz-target": amzTarget,
+        "Content-type": contentType,
+        "Authorization": authorization_header
+    };
+    
+    return [
+        requestHeaders,
+        host
+    ];
+}
+
+function processCheckCommand(access_key, secret_key, region, commandId, instanceId) {
+    var amzTarget = 'AmazonSSM.GetCommandInvocation';
+    var payload = {
+        "CommandId": commandId,
+        "InstanceId": instanceId
+    };
+    var strPayload = JSON.stringify(payload);
+    var blob = buildHeader(access_key, secret_key, region, amzTarget, strPayload);
+    var requestHeaders = blob[0];
+    const host = blob[1];
+
+    if (input.AWSSessionToken) {
+        requestHeaders["X-Amz-Security-Token"] = input.AWSSessionToken;
+    }
+    var ssmRequest = http.request({
+        endpoint: host,
+        path: "/",
+        method: 'POST',
+        headers: requestHeaders
+    });
+    var ssmResponse = ssmRequest.write(strPayload);
+    // If we call this too quickly, we can get an "invocation does not exist"
+    // error, so check for that explicitly and pretend we're pending ...
+    if (ssmResponse.statusCode === 400 && ssmResponse.body.startsWith('{"__type":"InvocationDoesNotExist"}')) {
+        return ["Pending", ""];
+    }
+    // For all other errors, we've failed.
+    if (ssmResponse.statusCode !== 200) {
+        return ["Failed", ssmResponse.body];
+    }
+    var ssmResponseBody = JSON.parse(ssmResponse.body);
+    var ssmStatus = ssmResponseBody.Status;
+    if (ssmStatus == "Failed") {
+        return [ssmStatus, ssmResponseBody.StandardErrorContent];
+    }
+    return [ssmStatus, ssmResponseBody.StandardOutputContent];
+}
+
+function processSendCommand(access_key, secret_key, region, instanceId, command) {
+    var amzTarget = 'AmazonSSM.SendCommand';
+    var payload = {
+        "DocumentName": "AWS-RunShellScript",
+        "InstanceIds": [instanceId],
+        "Parameters": {
+            "commands": [command]
+        }
+    };
+    var strPayload = JSON.stringify(payload);
+    var blob = buildHeader(access_key, secret_key, region, amzTarget, strPayload);
+    var requestHeaders = blob[0];
+    const host = blob[1];
+    
+    if (input.AWSSessionToken) {
+        requestHeaders["X-Amz-Security-Token"] = input.AWSSessionToken;
+    }
+    var ssmRequest = http.request({
+        endpoint: host,
+        path: "/",
+        method: 'POST',
+        headers: requestHeaders
+    });
+    var ssmResponse = ssmRequest.write(strPayload);
+    if (ssmResponse.statusCode !== 200) {
+        return [false, ssmResponse.body];
+    }
+    var ssmResponseBody = JSON.parse(ssmResponse.body);
+    return [ssmResponseBody.Command.CommandId, ssmResponseBody.Command.Status];
+}
+
+function runCommand(access_key, secret_key, region, instanceId, command) {
+    const desiredStates = ["Success", "Failed"];
+    var outcome = processSendCommand(access_key, secret_key, region, instanceId, command);
+    var id = outcome[0];
+    var status = outcome[1];
+    var output = status;
+    if (id) {
+        while (!desiredStates.includes(status)) {
+            var loopOutcome = processCheckCommand(access_key, secret_key, region, id, instanceId);
+            status = loopOutcome[0];
+            output = loopOutcome[1];
+        }
+    } else {
+        status = "Failed";
+    }
+    return [
+        (status === "Success"),
+        output
+    ];
+}
+
+try {
+    var device = input.EBSDevice;
+    var volSize = Number(input.CurrentSize);
+
+    var cmd = "BLOCKCOUNT=$(sudo dumpe2fs -h /dev/"+device+" | grep 'Block count' | awk '{print $3}') && BLOCKSIZE=$(sudo dumpe2fs -h /dev/"+device+" | grep 'Block size' | awk '{print $3}') && echo \"$(($BLOCKCOUNT * $BLOCKSIZE))\"";
+
+    var result = runCommand(input.AWSAccessKey, input.AWSSecretKey, input.AWSRegion, input.InstanceId, cmd);
+    if (!result[0]) {
+        throw new Error(result[1]);
+    }
+
+    // AWS has the volume size in GiB ... we've just retrieved the number of bytes, so convert ...
+    var fsSize = Math.ceil(Number(result[1]) / 1073741824);
+    // There may be a rounding error, so allow it to be within 1 of the current size
+    console.log((volSize-1)+" <= "+fsSize+" <= "+volSize);
+    if (volSize-1 <= fsSize && fsSize <= volSize) {
+        output.CommandSuccess = true;
+    } else {
+        output.CommandSuccess = false;
+        output.ErrorMessage = "It looks like the volume has been expanded but the filing system not resized. Need to handle this manually.";
+    }
+} catch (error) {
+    output.ErrorMessage = error.message;
+    output.CommandSuccess = false;
+}
